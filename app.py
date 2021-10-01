95553dde7840d644b8b8ce927ede93fb4c807900
yujeong lim <anneu5712@gmail.com>
2021-10-01 16:49:25 +0900

    yujeong second commit

--- app.py
+++ app.py
@@ -1,19 +1,20 @@
-
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 ## HTML 화면 보여주기
-@app.route('/')        ## home 페이지 - /
-def travle():          ## 우리의 여행일기 - travle
+@app.route('/')        ## home 페이지주소 : /
+def travle():          ## 우리의 여행일기 : travle
     return render_template('index.html')
 
-@app.route('/menu1')   ## 일기쓰기 페이지 - menu1
-def record():          ## 일기쓰기 - record
+@app.route('/write')   ## 일기쓰기 페이지주소 : /menu1 -> /write
+def write():           ## 일기쓰기 : record -> write
     return render_template('write.html')
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
 
 ## 2021.09.29 완료 !
+
+## 변수명 변경 제안 (일기쓰기주소 + 함수명 : memu1, record -> write)
