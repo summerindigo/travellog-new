21478e6aec677aa2a926f7e084cc36d5a2caf7a6
chris <chjeong7124@gmail.com>
2021-10-01 20:36:36 +0900

    write.html POST ajax & app.py API

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, jsonify, request 
 
 app = Flask(__name__)
 
 from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 # client = MongoClient('mongodb://test:test@localhost', 27017) 이건 일부러 비활성화 시킴
-db = client.travellog_project
+db = client. travellog_project
 
 from datetime import datetime   ## file 저장 명칭을 위해 필요
 
@@ -24,8 +24,8 @@ def record():          ## 일기쓰기 - record
 ## 포스트 작성 페이지 write.html
 @app.route('/menu1', methods=['POST'])
 def post():            ## 일기 저장 버튼 누르면
-    id_writer_receive = request.form['id_writer_give']
-    id_numbers_receive = request.form['id_numbers_give']
+    id_writer_receive = request.form['writer_give']
+    id_numbers_receive = request.form['numbers_give']
     place_receive = request.form['place_give']
     title_receive = request.form['title_give']
     ## date - 아래에 mydate 관련
@@ -37,7 +37,7 @@ def post():            ## 일기 저장 버튼 누르면
     extension = file.filename.split('.')[-1]  ## 확장자 이름 jpg png pdf 등
 
     today = datetime.now()
-    mytime = today.strftime('%Y-%m-%d-%H%M%S') ## 예시 2020-09-01-125956 -> 2020년 9월 1일 12시 59분 56초 인것
+    mytime = today.strftime('%Y%m%d-%H%M%S') ## 예시 20200901-125956 -> 2020년 9월 1일 12시 59분 56초 인것
     mydate = today.strftime('%Y-%m-%d') ## 달력에서 날짜 선택하면 2020-10-10 형식으로 나오도록
 
 
