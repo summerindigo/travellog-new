600a51a8db357f11bc89dbb5c3c1fb51e16bd6ba
junhee han <hanjun1227@naver.com>
2021-10-03 22:25:20 +0900

    success_robo3t_commit

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('localhost', 27017) # client = MongoClient('mongodb://test:test@localhost', 27017) 이건 일부러 비활성화 시킴
+# client = MongoClient('localhost', 27017)
+client = MongoClient('mongodb://test:test@15.164.50.230:27017')
 db = client.travellog_project
 
 from datetime import datetime   ## file 저장 명칭을 위해 필요
@@ -31,17 +32,17 @@ def write_log():            ## 일기 저장 버튼 누르면
     comment_receive = request.form['comment_give']
     print(request.data)
 
-    file = request.files['file_give']
-    extension = file.filename.split('.')[-1]  ## 확장자 이름 jpg png pdf 등
+    # file = request.files['file_give']
+    # extension = file.filename.split('.')[-1]  ## 확장자 이름 jpg png pdf 등
 
     today = datetime.now()
     mytime = today.strftime('%Y%m%d-%H%M%S') ## 예시 20200901-125956 -> 2020년 9월 1일 12시 59분 56초 인것
     mydate = today.strftime('%Y-%m-%d') ## 달력에서 날짜 선택하면 2020-10-10 형식으로 나오도록
 
 
-    filename = f'file-{mytime}'
-    save_to = f'static/{filename}.{extension}' ## static파일에 사진이 저장됨
-    file.save(save_to)
+    # filename = f'file-{mytime}'
+    # save_to = f'static/{filename}.{extension}' ## static파일에 사진이 저장됨
+    # file.save(save_to)
 
     doc = {
         'writer': id_writer_receive,
@@ -50,7 +51,7 @@ def write_log():            ## 일기 저장 버튼 누르면
         'title': title_receive,
         'date': mydate,
         'weather': weather_receive,
-        'file': f'{filename}.{extension}',
+        # 'file': f'{filename}.{extension}',
         'comment': comment_receive
     }
 
