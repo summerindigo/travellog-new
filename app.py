c6aac0cdf5db730b09ebb0b32ffe93b5fe23edeb
yujeong lim <anneu5712@gmail.com>
2021-10-03 16:55:07 +0900

    yujeong 4 commit

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, jsonify , request
 app = Flask(__name__)
-
 
 ## HTML 화면 보여주기
 @app.route('/')        ## 우리의 여행일기 주소 : /
-def home():          ## 우리의 여행일기 : home
+def home():            ## 우리의 여행일기 : home
     return render_template('index.html')
 
 @app.route('/write')   ## 일기쓰기 주소 : write
@@ -16,9 +14,15 @@ def write():           ## 일기쓰기 : write
 def mylog():           ## 내일기 모아보기 : mylog
     return render_template('mylog.html')
 
+
+## API 역할을 하는 부분
+@app.route('/log', methods=['POST'])
+def write_log():
+    test_receive = request.form['test_give']
+    print(test_receive)
+    return jsonify({'result': 'success', 'msg': '이 요청은 POST!'})
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
 
 
-
-## 변수명 변경 (주소 + 함수명 : memu1, record -> write / travle -> home)
