8d7ac4ac61df6a9ed164b4690bdec1087012d0d9
chris <chjeong7124@gmail.com>
2021-10-04 00:08:58 +0900

    ajax POST 성공

--- app.py
+++ app.py
@@ -63,5 +63,6 @@ def write_log():            ## 일기 저장 버튼 누르면
     return jsonify({'result': 'success', 'msg': '이 요청은 POST!'})
 
 
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
