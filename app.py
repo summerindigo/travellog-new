db639f9371d2d713f3061e60bd10c8b368ba1f80
yujeong lim <anneu5712@gmail.com>
2021-10-03 17:12:40 +0900

    yujeong 4 commit

--- app.py
+++ app.py
@@ -22,6 +22,7 @@ def write_log():
     print(test_receive)
     return jsonify({'result': 'success', 'msg': '이 요청은 POST!'})
 
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
 
